e12f849ea65e9380c359a36529103ddc557716d6
Serie Sumei <seriesumei@avimail.org>
2020-02-29 12:33:48 -0600

    Remove Extras migrated files

--- LSLScripts/HUD/HUD.sol
+++
@@ -1,4 +0,0 @@
-<Solution name="HUD">
-	<Project name="Roth" path="Roth\Roth.prj"/>
-	<Project name="Ruth" path="Ruth\Ruth.prj" active="true"/>
-</Solution>
--- LSLScripts/HUD/Roth/readme.txt
+++
@@ -1,1 +0,0 @@
-Current release of LSL Scripts for Roth 2.0 may be found in Mesh/Avatar Roth/Release/Scripts
